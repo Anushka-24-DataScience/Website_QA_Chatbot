1edc5f6de7bd8645fda4c216d3db81b4c15bb44c
Anushka-24-DataScience <shilpirbs@gmail.com>
2024-07-14 14:04:56 +0530

    requirements added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'WEBSITE_QA_Chatbot',
+    version= '0.0.0',
+    author= 'Anushka Srivastava',
+    author_email= 'anushka.asthana2@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
